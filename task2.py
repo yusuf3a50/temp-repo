dcf80874343d7498681183631a3d597dba9b2d80
yusuf3a50 <ybyphone@disroot.org>
2022-06-25 00:15:07 +0100

    posting the solution

--- task2.py
+++ task2.py
@@ -20,10 +20,7 @@
 # 7. However, when a user asks to ‘withdraw’ more money than they have on their
 # account, then you need to raise an error an exit the program.
 
-def request_pin():
-    attempt = input('please input your pin: ')
-
-def cash():
+def cash(withdrawal):
     balance = 100
     print(f'your balance is £{balance}')
     if balance < withdrawal:
@@ -34,24 +31,26 @@ def cash():
         print(f'Please take your cash. Your new balance is now £{balance}')
         return True
 
-# def pin(attempt):
-#     tries = 0
-#     while tries <3:
-#         if attempt != 1234:
-#             print('your pin was not correct')
-#             tries += 1
-#             return False
-#         if attempt == 1234:
-#             print('your pin was correct')
-#             cash()
-#             return True
-#         if tries == 3:
-#             print('your card has been blocked and you have been reported to the python police')
+def correct_pin(pin):
+    if pin == 1234:
+        return True
+    else:
+        return False
 
+def attempts(pin, tries):
+    while tries <3:
+        tries += 1
+        print(f'Youve had {tries}/3 tries at inputting your pin')
+        if tries >= 3:
+            print('your card has been blocked and you have been reported to the python police')
+            return False
+        if correct_pin(pin) == False:
+            print('please try again')
+            pin = int(input('please input your pin: '))
+        if correct_pin(pin) == True:
+            print('pin correct!')
+            withdrawal = int(input('how much would would you like to withdraw? '))
+            cash(withdrawal)
+            return True
 
-request_pin()
-
-def wd():
-    withdrawal = int(input('how much would would you like to withdraw? '))
-wd()
-# pin(attempt)
+# to run this please run run_task2.py
